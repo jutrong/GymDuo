698a2143c74e3e39ddeb48bd71f70cdc74aeeea8
[SW5] 이주현 <leek71358@gmail.com>
2023-11-11 22:58:10 +0900

    navigation native/native-stack 패키지 다운

---
+++ screen/signupScreen.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const SingupScreen = () => {
+  return;
+  <SignUpContainer>SignupScreen</SignUpContainer>;
+};
+
+export default SignupScrren;
+
+const SignUpContainer = styled.View``;
